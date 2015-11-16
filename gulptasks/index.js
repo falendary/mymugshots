d74605a1665e8dada516c096c32a9435e9f88e22
DopingGood <nektomakseem@yandex.ru>
2015-11-16 15:58:37 +0400

    'changes'

--- gulptasks/index.js
+++ gulptasks/index.js
@@ -30,6 +30,10 @@
     var jslint = require('gulp-jslint-simple');
     var uglify = require('gulp-uglify');
     var concat = require('gulp-concat');
+    // jade
+    var jade = require('gulp-jade');
+    // server
+    var connect = require('gulp-connect');
 
     // html
     var htmlmin = require('gulp-htmlmin');
@@ -51,11 +55,11 @@
     gulp.task('index-min-Fonts', function () {
 
         var pathToFonts = [
-            'src/' + appName + '/content/fonts/*'
+            'src/' + appName + '/content/fonts/*.{otf,eot,ttf,woff,woff2,eof,svg}'
         ];
 
         return gulp.src(pathToFonts)
-            .pipe(gulp.dest('dist/' + appName + '/content/fonts/*'));
+            .pipe(gulp.dest('dist/' + appName + '/content/fonts/'));
     });
 
     gulp.task('index-min-LESS',  function () {
@@ -77,7 +81,8 @@
             .pipe(csslint())
             .pipe(minifyCSS({keepBreaks: false, keepSpecialComments: false}))
             .pipe(rename({basename: 'main', suffix: '.min', extname: '.css'}))
-            .pipe(gulp.dest('dist/' + appName + '/content/css/'));
+            .pipe(gulp.dest('dist/' + appName + '/content/css/'))
+            .pipe(connect.reload());
     });
 
     gulp.task('index-min-JS', function () {
@@ -106,20 +111,18 @@
             .pipe(livereload());
     });
 
-    gulp.task('index-min-HTML', function () {
-
-        var pathToHTMLAll = [
-            'src/' + appName + '/**/*.html'
+    gulp.task('jade', function() {
+         var pathToHTMLAll = [
+            'src/' + appName + '/scripts/app/views/jade/**/*.jade', !'src/' + appName + '/scripts/app/views/jade/utils/*.jade'
         ];
-
+        // gulp.src(['src/index/scripts/app/views/jade/**/*.jade', 'src/index/scripts/app/views/jade/**/*.jade'])
         return gulp.src(pathToHTMLAll)
-            .pipe(htmlmin({collapseWhitespace: true}))
-            .on('error', function (error) {
-                console.error('\nError on JS minification: \n', error.toString());
-                this.emit('end');
-            })
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest('dist/' + appName + '/'));
+        .pipe(jade({
+            pretty: true
+        }))
+            .on('error', console.log)
+        .pipe(gulp.dest('src/index/scripts/app/views/'))
+        .pipe(connect.reload());
     });
 
     gulp.task('index-HTML-to-JS', function () {
@@ -150,14 +153,24 @@
 
     });
 
-    gulp.task('index-min-All', ['index-min-IndexHTML', 'index-min-LESS', 'index-min-JS', 'index-min-HTML', 'index-min-IMG']);
+    gulp.task('http-server', function() {
+        connect.server(
+        {
+            root: 'dist',
+            livereload: true
+        });
+    });
+
+    gulp.task('index-min-All', ['index-min-IndexHTML', 'index-min-LESS', 'index-min-JS', 'index-min-IMG', 'http-server', 'index-min-Fonts']);
 
     // Watchers
     gulp.task('index-watch-min', ['index-min-All'], function () {
-        livereload.listen();
+        livereload.listen(1452);
         gulp.watch('src/**/*.{css,less}', ['index-min-LESS']);
+        gulp.watch('src/**/*.jade', ['jade']);
         gulp.watch('src/**/*.js', ['index-min-JS']);
-        gulp.watch('src/**/*.html', ['index-HTML-to-JS', 'index-min-HTML']);
+        gulp.watch('src/**/*.html', ['index-HTML-to-JS']);
+        gulp.watch('src/index/content/fonts/*.{otf,eot,ttf,woff,woff2,eof,svg}', ['index-min-Fonts']);
+        livereload.reload(['landing.html'])
     });
-
 }());
