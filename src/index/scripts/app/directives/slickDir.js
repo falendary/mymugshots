9e9740707bb738690c5e3aa0b340b4be9cd4574a
DopingGood <nektomakseem@yandex.ru>
2015-11-18 00:14:47 +0400

    arrivals here

--- src/index/scripts/app/directives/slickDir.js
+++ src/index/scripts/app/directives/slickDir.js
@@ -5,21 +5,19 @@
 		return {
 			restrict: 'A',
 			link: function (scope, elem, attrs) {
-				var windowWth = window.innerWidth,
+				var windowWth = window.innerWidth;
 					if (windowWth >= 1200) {
-						attrs.slidesToShow = '4';
+						attrs.slidesToShow = '4'; // Cv
 						attrs.slidesToScroll = '4';
 					}
 					else if (windowWth < 1200) {
 						if (windowWth < 768) {
 							attrs.slidesToShow = '2';
 							attrs.slidesToScroll = '2';
-							console.log(attrs.slidesToScroll);
 						}
 						else {
 							attrs.slidesToShow = '3';
 							attrs.slidesToScroll = '3';
-							console.log(attrs.slidesToShow);
 						}
 					};
 			}
