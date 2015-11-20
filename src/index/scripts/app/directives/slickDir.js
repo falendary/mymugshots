0572bd6fbb727b73dd08319d23cea21932d1d976
DopingGood <nektomakseem@yandex.ru>
2015-11-20 21:47:30 +0400

    detail and carousel

--- src/index/scripts/app/directives/slickDir.js
+++ src/index/scripts/app/directives/slickDir.js
@@ -9,7 +9,7 @@
 			},
 			link: function (scope, elem, attrs) {
 				var windowWth = window.innerWidth;
-				var gallery = $('.gallery-slick');
+				var	gallery = $('.gallery-slick');
 
 				if (windowWth >= 1200) {
 					gallery.attr('slides-to-show', 4);
@@ -25,19 +25,60 @@
 						gallery.attr('slides-to-scroll', 2);
 					}
 				}
-				var swipeCounter = 0;
-				gallery.on('swipe', function(event, slick, direction) {
-					if (direction == 'left') {
-						swipeCounter -= 1;
-					}
-					else if (direction == 'right') {
-						swipeCounter += 1;
-					}
-					if (swipeCounter == 2 || swipeCounter == -2) {
-						swipeCounter = 0;
-						callBackFun();
-					}
+				// var swipeCounter = 0;
+				// gallery.on('swipe', function(event, slick, direction) {
+				// 	if (direction == 'left') {
+				// 		swipeCounter -= 1;
+				// 	}
+				// 	else if (direction == 'right') {
+				// 		swipeCounter += 1;
+				// 	}
+				// 	if (swipeCounter == 2 || swipeCounter == -2) {
+				// 		swipeCounter = 0;
+				// 		callBackFun();
+				// 	}
+				// });
+				var carEl = document.getElementsByClassName('ang-carousel')[0],
+					startM = 0,
+					moveLth = 0,
+					slideBase = 3,
+					swipeCounter = 0;
+				carEl.addEventListener('mousedown', function(e) {
+					startM = e.clientX;
+					carEl.onmousedown = function() {return false};
+					document.onselectstart = function() { return false };
+					carEl.ondragstart = function() { return false };
+					carEl.onmousemove = mouseMoveCar;
 				});
+				carEl.addEventListener('mouseup', mouseUpCar);
+				
+				function mouseMoveCar (e) {
+					moveLth = startM - e.clientX;
+				};
+				function mouseUpCar () {
+					if (moveLth > 104) {
+						swipeCounter = swipeCounter + 1;
+						console.log('move right');
+					}
+					else if (swipeCounter != 0 && moveLth < -104) {
+						swipeCounter = swipeCounter - 1;
+						console.log('move leftt');
+					};
+					if (swipeCounter == slideBase) {
+						swipeCallBack();
+						slideBase += 3;
+					};
+					console.log(swipeCounter);
+					carEl.onmousemove = null;
+					document.onselectstart = null;
+					carEl.ondragstart = null;
+					carEl.onmousedown = null;
+				};
+
+				function swipeCallBack () {
+					callBackFun();
+				};
+
 				function callBackFun() {
 					console.log('second slide');
 					scope.callback(); // - это колбек для закачки новых данных
